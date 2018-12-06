7b196b241e57662e805b5b560611a2821f0280a1
chad-marciniak <cmarciniak80@gmail.com>
2018-12-06 13:35:31 -0500

    adding member selection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import members from './members';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,14 +7,35 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      bandName: ''
+      bandName: '',
+      drummers: members.drummers,
+      guitarists: members.guitarists,
+      bassists: members.bassists,
+      drummer: undefined,
+      guitarist: undefined,
+      bassist: undefined,
     };
     this.changeBandName = this.changeBandName.bind(this);
+    this.selectGuitarist = this.selectGuitarist.bind(this);
+    this.selectDrummer = this.selectDrummer.bind(this);
+    this.selectBassist = this.selectBassist.bind(this);
+
   }
   
   changeBandName(event) {
     this.setState({bandName: event.target.value});
-    console.log(event.target.value);
+  }
+
+  selectGuitarist(event) {
+    this.setState({guitarist: event.target.innerHTML});
+  }
+
+  selectDrummer(event) {
+    this.setState({drummer: event.target.innerHTML});
+  }
+
+  selectBassist(event) {
+    this.setState({bassist: event.target.innerHTML});
   }
   
   render() {
@@ -25,20 +47,23 @@ class App extends Component {
         </div>
         <div>
           <span>Select guitarist</span>
-          <div> guitarists</div>
+          <div><ul>{this.state.guitarists.map(item => <li onClick={event => this.selectGuitarist(event)}>{item}</li>)}</ul></div>
         </div>
         <div>
           <span>Select durmmer</span>
-          <div> drummers</div>
+          <div><ul>{this.state.drummers.map(item => <li onClick={event => this.selectDrummer(event)}>{item}</li>)}</ul></div>
         </div>
         <div>
-          <span>Select singer</span>
-          <div> drummers</div>
+          <span>Select bassist</span>
+          <div><ul>{this.state.bassists.map(item => <li onClick={event => this.selectBassist(event)}>{item}</li>)}</ul></div>
         </div>
 
         <div>
           <span>The band: {this.state.bandName}</span>
           <div>band members</div>
+          <div><span>Guitarist: {this.state.guitarist}</span></div>
+          <div><span>Drummer: {this.state.drummer}</span></div>
+          <div><span>Bassist: {this.state.bassist}</span></div>
         </div>
       </div>
     );
---
+++ src/members.js
@@ -0,0 +1,28 @@
+const members = {
+  "drummers": [
+    "Vinnie Paul Abbott",
+    "Dave Abbruzzese",
+    "Matt Abts",
+    "Alex Acuna",
+    "Daniel Adair",
+    "Charlie Adams",
+  ],
+  "guitarists": [
+    "Johnny A",
+    "Abbath Doom Occulta",
+    "Dimebag",
+    "Drew Abbott",
+    "John Abercrombie",
+    "Aaron Abeyta",
+  ],
+  "bassists": [
+    "Wil Abers",
+    "Barry Adamson",
+    "Paul Abrahams",
+    "Gaye Advert",
+    "Martin Eric Ain",
+    "Dominic Aitchison"
+  ],
+};
+
+module.exports = members;
